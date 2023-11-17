51eab34e1898015f12597d1096095d8c6a62cb60
FlorexRDP1 <127148454+FlorexRDP1@users.noreply.github.com>
2023-11-17 18:23:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-global.owner = ['6281395861695']  
-global.mods = ['6281395861695'] 
-global.prems = ['6281395861695']
-global.nameowner = 'Tio'
-global.numberowner = '6281395861695' 
+global.owner = ['6287817207497']  
+global.mods = ['6287817207497'] 
+global.prems = ['6285810280139']
+global.nameowner = 'florexgt'
+global.numberowner = '6287817207497' 
 global.mail = 'support@tioprm.my.id' 
-global.gc = 'https://chat.whatsapp.com/Ln2vHjRrRayAbzalRMB56r'
-global.instagram = 'https://instagram.com/prm2.0'
-global.wm = '© Tio'
+global.gc = 'https://instagram.com/florexgt'
+global.instagram = 'https://instagram.com/florexgt'
+global.wm = '© florexgt'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
@@ -15,18 +15,18 @@ global.author = 'Bot WhatsApp'
 global.maxwarn = '2' // Peringatan maksimum
 
 //INI WAJIB DI ISI!//
-global.btc = 'YOUR_APIKEY_HERE' 
+global.btc = 'uwuMufXB' 
 //Daftar terlebih dahulu https://api.botcahx.live
 
 //INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
-global.lann = 'YOUR_APIKEY_HERE'
+global.lann = 'uwuMufXB'
 //Daftar https://api.betabotz.org 
 
 global.APIs = {   
   btc: 'https://api.botcahx.live'
 }
 global.APIKeys = { 
-  'https://api.botcahx.live': 'APIKEY' 
+  'https://api.botcahx.live': 'uwuMufXB' 
 }
 
 let fs = require('fs')
